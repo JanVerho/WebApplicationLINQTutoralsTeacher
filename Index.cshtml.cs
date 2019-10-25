24cc77651d6abdd9057097eba7d723530c1d143b
Jan <zoete.inval.bevel@gmail.com>
2019-10-25 02:08:11 +0200

    opgehaalde files van c:/....

---
+++ Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplicationLINQTutoralsTeacher.Pages
+{
+    //Delegates before & out of Classes!!
+    public delegate bool IsTeenAger(Student stud);
+    public delegate bool IsYoungerThan(Student stud, int youngAge);
+    public delegate void Printing();
+
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public String StudentName { get; set; }
+        public int Age { get; set; }
+
+
+    }
+}
